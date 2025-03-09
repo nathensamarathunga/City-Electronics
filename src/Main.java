2f37de3c2d2c02ed1041aa961bfff744d2bfaa94
Nathen Samarathunga <thisisnathen@outlook.com>
2025-03-09 15:49:35 +0530

    Implemented Customer & Employee login functionality

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 class Main {
 
     public static boolean loginState = false;
 
+    public static String[][] userCredentials = {{"null", "null"}};
+
+    public static Scanner input = new Scanner(System.in);
+
+    public static int i;
+
     public static void loginPage() {
-        Scanner input = new Scanner(System.in);
         int valid = 1;
         int option = 0;
 
@@ -30,16 +36,90 @@ class Main {
     }
 
     public static void customerCheckLogin(){
+
+        i = 0;
+
+        String check;
+
         String[][] users = { {"Ranil", "Ranil@123"}, {"Kamal", "Kamal@123"}, {"Mahinda", "Mahinda@123"} };
 
         System.out.println("Customer Login Under Construction");
 
+        System.out.print("Enter Username\t: ");
+        userCredentials[0][0] = input.next();
+
+        System.out.print("Enter Password\t: ");
+        userCredentials[0][1] = input.next();
+
+        while ((loginState == false) && i < 2) {
+
+            if ((Objects.equals(users[i][0], userCredentials[0][0])) && (Objects.equals(users[i][1], userCredentials[0][1]))) {
+                loginState = true;
+                break;
+            }
+            else
+                i++;
+        }
+
+        if (loginState == false) {
+            System.out.println("Invalid Login Information");
+            System.out.println("Do you want to try again? (Y/N)\t: ");
+            check = input.next();
+
+            if (check == "Y")
+                customerCheckLogin();
+            else
+                loginPage();
+
+        } else {
+            System.out.println("Successfully Logged In!");
+            customerActions();
+        }
+
+
+
     }
 
     public static void employeeCheckLogin(){
+
+        i = 0;
+
+        String check;
+
         String[][] users = { {"Admin", "Ad@123"}, {"Manager", "Mgr@123"}, {"Employee", "Emp@123"} };
 
         System.out.println("Employee Login Under Construction");
+
+        System.out.print("Enter Username\t: ");
+        userCredentials[0][0] = input.next();
+
+        System.out.print("Enter Password\t: ");
+        userCredentials[0][1] = input.next();
+
+        while ((loginState == false) && i < 2) {
+
+            if ((Objects.equals(users[i][0], userCredentials[0][0])) && (Objects.equals(users[i][1], userCredentials[0][1]))) {
+                loginState = true;
+                break;
+            }
+            else
+                i++;
+        }
+
+        if (loginState == false) {
+            System.out.println("Invalid Login Information");
+            System.out.println("Do you want to try again? (Y/N)\t: ");
+            check = input.next();
+
+            if (check == "Y")
+                employeeCheckLogin();
+            else
+                loginPage();
+
+        } else {
+            System.out.println("Successfully Logged In!");
+            employeeActions();
+        }
     }
 
     public static void customerActions() {
